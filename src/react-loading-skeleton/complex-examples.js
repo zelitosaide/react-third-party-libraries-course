d2921f3c83dc75b18d4c75837b11ce19fd978e1b
zelitosaide <zelitosaide83@gmail.com>
2022-12-16 13:31:53 +0200

    React Loading Skeleton

--- src/react-loading-skeleton/complex-examples.js
+++ src/react-loading-skeleton/complex-examples.js
@@ -6,30 +6,24 @@ export function ComplexExamples() {
   const [loading, setLoading] = useState(true);
 
   return (
-    <div style={{ display: "flex", width: 600, background: "red" }}>
-      <div
-        style={{
-          width: 75,
-          background: "green",
-          display: "flex",
-          marginRight: "1.5rem",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <div
-          style={{
-            width: 55,
-            height: 55,
-            background: "#eaeaea",
-            borderRadius: "50%",
-          }}
-        >
+    <div className="post">
+      <div className="left-col">
+        <div className="avatar">
+          {loading && (
+            <Skeleton
+              circle
+              height="100%"
+              containerClassName="avatar-skeleton"
+            />
+          )}
           <img
             src="smile.svg"
             alt="A user avatar"
             style={{ display: loading ? "none" : undefined }}
           />
+        </div>
+        <div className="user-name">
+          {loading ? <Skeleton width={70} /> : "John Doe"}
         </div>
       </div>
     </div>
