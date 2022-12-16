534cfd214d7a97d3814ffb1f4125ac2cff2ac9b0
zelitosaide <zelitosaide83@gmail.com>
2022-12-16 13:08:27 +0200

    React Loading Skeleton

--- src/react-loading-skeleton/complex-examples.js
+++ src/react-loading-skeleton/complex-examples.js
@@ -2,5 +2,20 @@ import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
 export function ComplexExamples() {
-  return <div className="post"></div>;
+  return (
+    <div style={{ display: "flex", width: 600, background: "red" }}>
+      <div
+        style={{
+          width: 75,
+          background: "green",
+          display: "flex",
+          marginRight: "1.5rem",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        ola
+      </div>
+    </div>
+  );
 }
