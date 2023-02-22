898a716abb44d39060d82b85b9281124bfa174c0
zelitosaide <zelitosaide83@gmail.com>
2023-02-22 11:40:58 +0200

    React Quill

---
+++ src/react-quill/index.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+export default function Index() {
+  const [value, setValue] = useState("");
+
+  return (
+    <ReactQuill
+      theme="snow"
+      value={value}
+      onChange={setValue}
+    />
+  );
+}
