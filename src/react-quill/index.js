9d67f5f131fd174d6312a94a1e8ce6ff4b52eb91
zelitosaide <zelitosaide83@gmail.com>
2023-02-22 11:56:45 +0200

    React Quill

--- src/react-quill/index.js
+++ src/react-quill/index.js
@@ -4,15 +4,16 @@ import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 
 export default function Index() {
-  const [value, setValue] = useState("");
-
-  console.log(value);
+  const [value, setValue] = useState("<h2>Ola</h2>");
 
   return (
-    <ReactQuill
-      theme="snow"
-      value={value}
-      onChange={setValue}
-    />
+    <>
+      <ReactQuill
+        theme="snow"
+        value={value}
+        onChange={setValue}
+      />
+      <div dangerouslySetInnerHTML={{ __html: value }}></div>
+    </>
   );
 }
