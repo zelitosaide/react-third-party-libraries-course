24ea64c54c1db5d484c9ae86a82907f2aa6a5e18
zelitosaide <zelitosaide83@gmail.com>
2023-03-04 21:42:53 +0200

    MUI

--- src/mui/select.js
+++ src/mui/select.js
@@ -1,24 +1,26 @@
 import { useState } from "react";
-import { Box, MenuItem, TextField } from "@mui/material";
+import { Box, MenuItem, Stack, TextField } from "@mui/material";
 
 export default function Index() {
   const [country, setCountry] = useState("");
 
   return (
-    <Box width={250}>
-      <TextField
-        label="Select country"
-        select
-        fullWidth
-        value={country}
-        onChange={function (e) {
-          setCountry(e.target.value);
-        }}
-      >
-        <MenuItem value="IN">India</MenuItem>
-        <MenuItem value="AU">Australia</MenuItem>
-        <MenuItem value="US">USA</MenuItem>
-      </TextField>
-    </Box>
+    <Stack spacing={4}>
+      <Box width={250}>
+        <TextField
+          label="Select country"
+          select
+          fullWidth
+          value={country}
+          onChange={function (e) {
+            setCountry(e.target.value);
+          }}
+        >
+          <MenuItem value="IN">India</MenuItem>
+          <MenuItem value="AU">Australia</MenuItem>
+          <MenuItem value="US">USA</MenuItem>
+        </TextField>
+      </Box>
+    </Stack>
   );
 }
