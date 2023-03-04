519e918bad6f9447844a11f37c8c31b52693e242
zelitosaide <zelitosaide83@gmail.com>
2023-03-04 21:38:43 +0200

    MUI

--- src/mui/select.js
+++ src/mui/select.js
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import { Box, MenuItem, TextField } from "@mui/material";
 
 export default function Index() {
+  const [country, setCountry] = useState("");
+
   return (
     <Box width={250}>
       <TextField
         label="Select country"
         select
+        fullWidth
       >
         <MenuItem value="IN">India</MenuItem>
         <MenuItem value="AU">Australia</MenuItem>
