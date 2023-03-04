9471bed685a6635c4b5cf91edc73d43d2e17f1bf
zelitosaide <zelitosaide83@gmail.com>
2023-03-04 15:26:58 +0200

    MUI

--- src/mui/typography.js
+++ src/mui/typography.js
@@ -6,7 +6,12 @@ export default function Index() {
       <Typography variant="h1">H1 heading</Typography>
       <Typography variant="h2">H2 heading</Typography>
       <Typography variant="h3">H3 heading</Typography>
-      <Typography variant="h4">H4 heading</Typography>
+      <Typography
+        variant="h4"
+        component="h1"
+      >
+        H4 heading
+      </Typography>
       <Typography variant="h5">H5 heading</Typography>
       <Typography variant="h6">H6 heading</Typography>
       <Typography variant="subtitle1">Sub title 1</Typography>
