118a26f5ef00240c515368e5a8096932f2f1bdc3
zelitosaide <zelitosaide83@gmail.com>
2023-03-04 20:17:43 +0200

    MUI

--- src/mui/index.js
+++ src/mui/index.js
@@ -1,9 +1,11 @@
 import Tipografias from "./typography";
 import Butoes from "./button";
+import TextFields from "./text-field";
 
 export default function Index() {
   return (
     <>
+      <TextFields />
       <Butoes />
       <Tipografias />
     </>
