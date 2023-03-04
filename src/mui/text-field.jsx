a3312125ea343ee79281845f4905681cd71be9cd
zelitosaide <zelitosaide83@gmail.com>
2023-03-04 20:20:06 +0200

    MUI

--- src/mui/text-field.jsx
+++ src/mui/text-field.jsx
@@ -3,7 +3,10 @@ import { Stack } from "@mui/material";
 export default function Index() {
   return (
     <Stack spacing={4}>
-      <Stack direction="row"></Stack>
+      <Stack
+        direction="row"
+        spacing={2}
+      ></Stack>
     </Stack>
   );
 }
