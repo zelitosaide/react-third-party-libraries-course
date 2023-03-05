c656cc01c909a873bf3d707df1e831841c644f2d
zelitosaide <zelitosaide83@gmail.com>
2023-03-05 08:10:16 +0200

    MUI

--- src/mui/index.js
+++ src/mui/index.js
@@ -2,10 +2,12 @@ import Tipografias from "./typography";
 import Butoes from "./button";
 import TextFields from "./text-field";
 import SelectFields from "./select";
+import RadioButtons from "./radio-buttons";
 
 export default function Index() {
   return (
     <>
+      <RadioButtons />
       <SelectFields />
       <TextFields />
       <Butoes />
