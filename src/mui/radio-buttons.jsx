3d3e92ffc771668d72024524b17c722c3e448a62
zelitosaide <zelitosaide83@gmail.com>
2023-03-05 08:27:57 +0200

    MUI

--- src/mui/radio-buttons.jsx
+++ src/mui/radio-buttons.jsx
@@ -25,6 +25,7 @@ export default function Index() {
           onChange={function (e) {
             setValue(e.target.value);
           }}
+          row
         >
           <FormControlLabel
             control={<Radio />}
