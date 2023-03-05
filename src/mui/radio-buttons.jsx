97bb12cdacd28d09b6ea70e08ff40ed86a2c0f21
zelitosaide <zelitosaide83@gmail.com>
2023-03-05 08:24:07 +0200

    MUI

--- src/mui/radio-buttons.jsx
+++ src/mui/radio-buttons.jsx
@@ -23,6 +23,16 @@ export default function Index() {
             label="0-2"
             value="0-2"
           />
+          <FormControlLabel
+            control={<Radio />}
+            label="3-5"
+            value="3-5"
+          />
+          <FormControlLabel
+            control={<Radio />}
+            label="6-10"
+            value="6-10"
+          />
         </RadioGroup>
       </FormControl>
     </Box>
