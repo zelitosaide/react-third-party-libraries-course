dd614cbdbefe20faf8115e15b8c226daabd526d5
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-19 10:54:06 +0200

    Navigation

--- src/swiperjs/navigation/page.js
+++ src/swiperjs/navigation/page.js
@@ -11,7 +11,7 @@ import image2 from "../images/image-2.jpg";
 import image3 from "../images/image-3.jpeg"; 
 import image4 from "../images/image-4.jpg"; 
 import image5 from "../images/image-5.jpg"; 
-import image6 from "../images/image-6.jpg"; 
+import image6 from "../images/image-6.jpeg"; 
 import image7 from "../images/image-7.jpg"; 
 import image8 from "../images/image-8.jpeg"; 
 import image9 from "../images/image-9.jpg"; 
@@ -21,30 +21,75 @@ export default function Page() {
     <Swiper navigation={true} modules={[Navigation]}>
       <SwiperSlide>
         <img src={image1} />
+        <p>
+          Notice the use of %PUBLIC_URL% in the tags above.
+          It will be replaced with the URL of the `public` folder during the build.
+          Only files inside the `public` folder can be referenced from the HTML.
+        </p>
       </SwiperSlide>
       <SwiperSlide>
         <img src={image2} />
+        <p>
+          Notice the use of %PUBLIC_URL% in the tags above.
+          It will be replaced with the URL of the `public` folder during the build.
+          Only files inside the `public` folder can be referenced from the HTML.
+        </p>
       </SwiperSlide>
       <SwiperSlide>
         <img src={image3} />
+        <p>
+          Notice the use of %PUBLIC_URL% in the tags above.
+          It will be replaced with the URL of the `public` folder during the build.
+          Only files inside the `public` folder can be referenced from the HTML.
+        </p>
       </SwiperSlide>
       <SwiperSlide>
         <img src={image4} />
+        <p>
+          Notice the use of %PUBLIC_URL% in the tags above.
+          It will be replaced with the URL of the `public` folder during the build.
+          Only files inside the `public` folder can be referenced from the HTML.
+        </p>
       </SwiperSlide>
       <SwiperSlide>
         <img src={image5} />
+        <p>
+          Notice the use of %PUBLIC_URL% in the tags above.
+          It will be replaced with the URL of the `public` folder during the build.
+          Only files inside the `public` folder can be referenced from the HTML.
+        </p>
       </SwiperSlide>
       <SwiperSlide>
         <img src={image6} />
+        <p>
+          Notice the use of %PUBLIC_URL% in the tags above.
+          It will be replaced with the URL of the `public` folder during the build.
+          Only files inside the `public` folder can be referenced from the HTML.
+        </p>
       </SwiperSlide>
       <SwiperSlide>
         <img src={image7} />
+        <p>
+          Notice the use of %PUBLIC_URL% in the tags above.
+          It will be replaced with the URL of the `public` folder during the build.
+          Only files inside the `public` folder can be referenced from the HTML.
+        </p>
       </SwiperSlide>
       <SwiperSlide>
         <img src={image8} />
+        <p>
+          Notice the use of %PUBLIC_URL% in the tags above.
+          It will be replaced with the URL of the `public` folder during the build.
+          Only files inside the `public` folder can be referenced from the HTML.
+        </p>
       </SwiperSlide>
       <SwiperSlide>
         <img src={image9} />
+        <p>
+          Notice the use of %PUBLIC_URL% in the tags above.
+          It will be replaced with the URL of the `public` folder during the build.
+          Only files inside the `public` folder can be referenced from the HTML.
+        </p>
       </SwiperSlide>
     </Swiper>
   );
