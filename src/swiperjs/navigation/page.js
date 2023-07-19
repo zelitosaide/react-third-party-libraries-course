ac6109390c7487190af1469e67341d5518612596
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-19 09:56:19 +0200

    Navigation

--- src/swiperjs/navigation/page.js
+++ src/swiperjs/navigation/page.js
@@ -6,18 +6,46 @@ import "swiper/css/navigation";
 
 import "./styles.css";
 
+import image1 from "../images/image-1.jpeg"; 
+import image2 from "../images/image-2.jpeg"; 
+import image3 from "../images/image-3.jpeg"; 
+import image4 from "../images/image-4.jpeg"; 
+import image5 from "../images/image-5.jpeg"; 
+import image6 from "../images/image-6.jpeg"; 
+import image7 from "../images/image-7.jpeg"; 
+import image8 from "../images/image-8.jpeg"; 
+import image9 from "../images/image-9.jpeg"; 
+
 export default function Page() {
   return (
     <Swiper navigation={true} modules={[Navigation]}>
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      <SwiperSlide>Slide 5</SwiperSlide>
-      <SwiperSlide>Slide 6</SwiperSlide>
-      <SwiperSlide>Slide 7</SwiperSlide>
-      <SwiperSlide>Slide 8</SwiperSlide>
-      <SwiperSlide>Slide 9</SwiperSlide>
+      <SwiperSlide>
+        <img src={image1} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={image2} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={image3} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={image4} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={image5} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={image6} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={image7} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={image8} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={image9} />
+      </SwiperSlide>
     </Swiper>
   );
 }
